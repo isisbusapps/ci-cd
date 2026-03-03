028cba6f37ec61a3ede9daac78d78c15f5ac4cfc
David Symons <david.symons@stfc.ac.uk>
2026-03-03 12:30:46 +0000

    Fix Sneaky typo

--- self-hosted-gh-runners/runner-image/packer/scripts/job_started.sh
+++ self-hosted-gh-runners/runner-image/packer/scripts/job_started.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Update Permissions in GH Action Workspace"
 WORK_DIR=/home/ubuntu/actions-runner/_work
-sudo chwon -R ubuntu:ubuntu $WORK_DIR
+sudo chown -R ubuntu:ubuntu $WORK_DIR
 sudo chmod -R u+rwX $WORK_DIR
 
-echo "Updated Permissions in GH Action Workspace
+echo "Updated Permissions in GH Action Workspace"
