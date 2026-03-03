e80c72557d5cc7304b4c22c82d61d621e866afb7
David Symons <david.symons@stfc.ac.uk>
2026-03-03 12:26:38 +0000

    Update startup script

--- self-hosted-gh-runners/runner-image/packer/scripts/job_started.sh
+++ self-hosted-gh-runners/runner-image/packer/scripts/job_started.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-echo "Clean GH Action Workspace"
+echo "Update Permissions in GH Action Workspace"
 WORK_DIR=/home/ubuntu/actions-runner/_work
 sudo chwon -R ubuntu:ubuntu $WORK_DIR
-sudo chmod -R u+w $WORK_DIR
+sudo chmod -R u+rwX $WORK_DIR
 
-find "$WORK_DIR" -mindept 1 -delete 2
-echo "Cleaned Workspace
+echo "Updated Permissions in GH Action Workspace
