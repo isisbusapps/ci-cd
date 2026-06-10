93d6a3333fcef9ab58bcdd44a28b7c356b2dcaea
David Symons <david.symons@stfc.ac.uk>
2026-06-10 13:42:47 +0100

    Move from ansible playbook

--- self-hosted-gh-runners/scripts/packer-scripts/setup_gh_runner_vm.sh
+++ self-hosted-gh-runners/scripts/packer-scripts/setup_gh_runner_vm.sh
@@ -7,6 +7,15 @@ sudo apt-get install -y ansible
 
 ansible --version 
 
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.36/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.36/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubectl
+
 sudo chmod +x ~/runner-scripts/job_started.sh
 sudo chmod +x ~/runner-scripts/job_completed.sh
 sudo chown ubuntu:ubuntu ~/runner-scripts/job_started.sh
